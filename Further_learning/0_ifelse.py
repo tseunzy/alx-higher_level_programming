987433048098ce79230b4b434114a0ad33a6037e
Oluwaseun Ogundare <tseunzy@yahoo.com>
2025-11-30 11:57:15 -0800

    added grade work

--- Further_learning/0_ifelse.py
+++ Further_learning/0_ifelse.py
@@ -6,10 +6,27 @@ if age >= 18:
 else:
     print("You are a minor")
 
-score = 75
-if score >= 70:
-    print("You Passed")
+
+#to lookout for even and odd numbers
+n = 11
+if n % 2 == 0:
+    print('Number is divisible by 2')
+elif n % 3 == 0:
+    print('Number is divisible by 3')
 else:
-    print("You Failed")
+    print('Number is neither divisible by 2 nor 3')
+print('Done!')
 
 
+#Grading of students scores using the if, elif 
+score = int(input("Enter your score: "))
+if score >= 70:
+    print("Excellent! Grade A")
+elif score >= 60:
+    print("Very Good! Grade B")
+elif score >= 50:
+    print("Good! Grade C")
+elif score >= 40:
+    print("Pass! Grade D")
+else:
+    print("Fail! Grade F")
