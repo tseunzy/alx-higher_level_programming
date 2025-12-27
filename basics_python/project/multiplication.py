41bc2ee83a47e8e396e2e5ea5763f14cc28a0aba
Oluwaseun Ogundare <tseunzy@yahoo.com>
2025-12-27 11:14:31 -0800

    Removed unused file

--- basics_python/project/multiplication.py
+++
@@ -1,9 +0,0 @@
-
-# a multiplication table 
-
-number = int(input('Enter a number: '))
-
-for i in range(1, 13):
-    print(f'{number:2} x {i:2} = {number * i:3}') #i had to format the :4width for better alignment
-
-
